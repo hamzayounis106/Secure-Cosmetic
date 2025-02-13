d35faf8fea9f688f1778900f07f1b9f0ef95d345
Ahmad Mamoon <ahmadeveloper077@gmail.com>
2025-02-13 06:05:31 +0500

    first commit

---
+++ src/lib/pusher.ts
@@ -0,0 +1,41 @@
+import PusherServer from 'pusher';
+import PusherClient from 'pusher-js';
+
+const CLUSTER = process.env.NEXT_PUBLIC_PUSHER_CLUSTER || 'ap2';
+console.log('Pusher Config:', {
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
+  secret: process.env.PUSHER_SECRET,
+});
+
+declare global {
+  var pusherServerInstance: PusherServer | undefined;
+  var pusherClientInstance: PusherClient | undefined;
+}
+
+if (!global.pusherServerInstance) {
+  global.pusherServerInstance = new PusherServer({
+    appId: process.env.PUSHER_APP_ID!,
+    key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+    secret: process.env.PUSHER_SECRET!,
+    cluster: CLUSTER,
+    useTLS: true,
+  });
+}
+
+if (!global.pusherClientInstance) {
+  global.pusherClientInstance = new PusherClient(
+    process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+    {
+      cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+      channelAuthorization: {
+        endpoint: '/api/pusher-auth',
+        transport: 'ajax',
+      },
+      forceTLS: true, // Add this
+    }
+  );
+}
+
+export const pusherServer = global.pusherServerInstance;
+export const pusherClient = global.pusherClientInstance;
